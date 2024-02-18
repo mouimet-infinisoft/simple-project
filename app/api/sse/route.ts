525d3660e1ef43cbf864cda810708fa634b5dca8
Martin Ouimet <mouimet@infinisoft.world>
2024-02-18 14:21:02 -0500

    .

--- app/api/sse/route.ts
+++ app/api/sse/route.ts
@@ -1,25 +1,30 @@
 // Filename: pages/api/sse.ts
 
-import type { NextApiRequest, NextApiResponse } from "next";
+// import type { NextApiRequest, NextApiResponse } from "next";
 
-// This is the handler function for your SSE route
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
+export const runtime = 'edge'
+ 
+export async function GET(req: Request) {
+  // res.setHeader('Content-Type', 'text/event-stream');
+  // res.setHeader('Cache-Control', 'no-cache');
+  // res.setHeader('Connection', 'keep-alive');
 
-  const sendEvent = (data: object) => {
-    res.write(`data: ${JSON.stringify(data)}\n\n`);
-  };
+  // const sendEvent = (data: object) => {
+  //   res.write(`data: ${JSON.stringify(data)}\n\n`);
+  // };
 
-  sendEvent({ message: 'Connection established' });
+  // sendEvent({ message: 'Connection established' });
 
-  const intervalId = setInterval(() => {
-    sendEvent({ time: new Date().toISOString() });
-  }, 1000);
+  // const intervalId = setInterval(() => {
+  //   sendEvent({ time: new Date().toISOString() });
+  // }, 1000);
 
   // req.on('close', () => {
   //   clearInterval(intervalId);
   //   res.end();
   // });
+
+  return new Response('Success!', {
+    status: 200,
+  })
 }
