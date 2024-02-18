149412fc2b4ad33346b4225dbd2cf35799c609ba
Martin Ouimet <mouimet@infinisoft.world>
2024-02-18 13:29:43 -0500

    chnage dir

---
+++ app/api/sse.ts
@@ -0,0 +1,26 @@
+// Filename: pages/api/sse.ts
+import { NextApiRequest, NextApiResponse } from 'next';
+
+// This is the handler function for your SSE route
+const sseHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+
+  const sendEvent = (data: object) => {
+    res.write(`data: ${JSON.stringify(data)}\n\n`);
+  };
+
+  sendEvent({ message: "Connection established" });
+
+  const intervalId = setInterval(() => {
+    sendEvent({ time: new Date().toISOString() });
+  }, 1000);
+
+  req.on('close', () => {
+    clearInterval(intervalId);
+    res.end();
+  });
+};
+
+export default sseHandler;
