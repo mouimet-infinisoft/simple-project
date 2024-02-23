aa1f4e44c7fc35a1a897ef15c7b49b3ce28640ff
Martin Ouimet <mouimet@infinisoft.world>
2024-02-23 05:56:17 -0500

    add lchat link

--- components/ui/Navbar/Navlinks.tsx
+++ components/ui/Navbar/Navlinks.tsx
@@ -26,9 +26,14 @@ export default function Navlinks({ user }: NavlinksProps) {
             Pricing
           </Link>
           {user && (
-            <Link href="/account" className={s.link}>
-              Account
-            </Link>
+            <>
+              <Link href="/account" className={s.link}>
+                Account
+              </Link>
+              <Link href="/chat" className={s.link}>
+                iBrain Chat
+              </Link>
+            </>
           )}
         </nav>
       </div>
