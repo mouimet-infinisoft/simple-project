e43c52d9e6e12118205d4bd3107c8a4e9114de99
Martin Ouimet <mouimet@infinisoft.world>
2024-02-25 03:13:38 -0500

    audio

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -15,11 +15,35 @@ const subscribeInsertMessage = () => {
       (payload) => {
         if (String(payload?.new?.role).includes('ibrain')) {
           console.log('Change received!', payload);
+          speak(String(payload?.new?.text))
         }
       }
     )
     .subscribe();
 };
+
+const speak = (text:string) => {
+  const sentences = text.split(/(?<=[.!?])/);
+
+  sentences.forEach((sentence) => {
+    const trimmedSentence = sentence.trim();
+    if (trimmedSentence) {
+      const utterance = new SpeechSynthesisUtterance(trimmedSentence);
+      utterance.lang = "en";
+      utterance.voice = window.speechSynthesis.getVoices()[87];
+
+      utterance.onstart = () => {
+        // bstack.store.emit("audio.stopListening");
+      };
+
+      utterance.onend = () => {
+        // bstack.store.emit("audio.startListening");
+      };
+
+      window.speechSynthesis.speak(utterance);
+    }
+  });
+}
 
 export default function ChatPage() {
   useEffect(() => {
