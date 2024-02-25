532abe33cf669dcb774f907e667a6692d454cd40
Martin Ouimet <mouimet@infinisoft.world>
2024-02-25 02:53:13 -0500

    filter

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -7,16 +7,18 @@ import { createClient } from '@/utils/supabase/client';
 const subscribeInsertMessage = () => {
   const supabase = createClient();
 
-
-  const channels = supabase.channel('custom-insert-channel')
-  .on(
-    'postgres_changes',
-    { event: 'INSERT', schema: 'public', table: 'messages', filter:"role=ibrain" },
-    (payload) => {
-      console.log('Change received!', payload)
-    }
-  )
-  .subscribe()
+  const channels = supabase
+    .channel('custom-insert-channel')
+    .on(
+      'postgres_changes',
+      { event: 'INSERT', schema: 'public', table: 'messages' },
+      (payload) => {
+        if (String(payload?.new?.role).includes('ibrain')) {
+          console.log('Change received!', payload);
+        }
+      }
+    )
+    .subscribe();
 };
 
 export default function ChatPage() {
