4d6d35d36cf79d848422665797b26f51a44b2d1a
Martin Ouimet <mouimet@infinisoft.world>
2024-02-25 14:19:30 -0500

    fix assertion

--- app/hooks/useSpeech2text.ts
+++ app/hooks/useSpeech2text.ts
@@ -20,22 +20,22 @@ const useSpeech2text = (onTrigger: (speech: string) => Promise<void>) => {
   }, []);
 
   useEffect(() => {
-    if (!('SpeechRecognition' in window)) {
+    if (!SpeechRecognition) {
       console.error('Speech Recognition is not supported by this browser.');
       return;
     }
 
-    recognition.current = new SpeechRecognition()
+    recognition.current = new SpeechRecognition();
     recognition.current.continuous = true;
     recognition.current.lang = 'en-US';
-    recognition.current.onresult = async (event:any) => {
+    recognition.current.onresult = async (event: any) => {
       const transcript = Array.from(event.results)
-        .map((result:any) => result?.[0]?.transcript)
+        .map((result: any) => result?.[0]?.transcript)
         .join('');
       await onTrigger(transcript);
     };
 
-    recognition.current.onerror = (event:any) => {
+    recognition.current.onerror = (event: any) => {
       console.error('Speech recognition error', event);
     };
 
@@ -55,14 +55,8 @@ const useSpeech2text = (onTrigger: (speech: string) => Promise<void>) => {
 
 export default useSpeech2text;
 
-
-
-
-
 // // useSpeech2text.ts
 // import { useCallback, useEffect, useRef, useState } from 'react';
-
-
 
 // const useSpeech2text = (onTrigger: (speech: string) => Promise<void>) => {
 //   const [isRecognizing, setIsRecognizing] = useState<boolean>(false);
