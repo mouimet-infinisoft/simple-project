a1d888d086f5bebbf090ecdad446d5ef7565140f
Martin Ouimet <mouimet@infinisoft.world>
2024-02-25 07:20:18 -0500

    logo

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 import logo from './logo.png'; // Make sure the path to your logo is correct
 
-const Logo = () => (
-  <Image src={logo} alt="Logo" width={32} height={32} /> // Adjust width and height as needed
+const Logo = ({width=32,height=32}:{width?:number,height?:number}) => (
+  <Image src={logo} alt="Logo" width={width} height={height} /> // Adjust width and height as needed
 );
 
 export default Logo;
