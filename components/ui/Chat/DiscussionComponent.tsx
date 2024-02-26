cbd031b223c579805fd4876e91c9eadc705d1f39
Martin Ouimet <mouimet@infinisoft.world>
2024-02-26 10:18:50 -0500

    delay

--- components/ui/Chat/DiscussionComponent.tsx
+++ components/ui/Chat/DiscussionComponent.tsx
@@ -3,33 +3,31 @@ import MessageComponent from './MessageComponent';
 import { ChatMessage } from 'types/ChatMessage'; // Adjust the import path as necessary
 
 const DiscussionComponent = ({ messages }: { messages: ChatMessage[] }) => {
-  // Ref for the messages container
   const containerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    // Introduce a slight delay to allow for the initial loading message rendering
-    const timer = setTimeout(() => {
-      const scrollHeight = containerRef.current?.scrollHeight ?? 0;
-      const height = containerRef.current?.clientHeight ?? 0;
-  
-      // Scroll to the bottom of the message container
-      containerRef.current?.scrollTo(0, scrollHeight - height);
-    }, 5500); // Adjust delay as needed
-  
-    return () => clearTimeout(timer); // Cleanup the timer on component unmount
-  }, []);
-
-  useEffect(() => {
-    // Scroll the container to the bottom
+  // Method to handle scrolling to the bottom
+  const scrollToBottom = () => {
     const scrollHeight = containerRef.current?.scrollHeight ?? 0;
     const height = containerRef.current?.clientHeight ?? 0;
-
-    // Set the scrollTop to the height of the container minus the visible height
     containerRef.current?.scrollTo(0, scrollHeight - height);
-  }, [messages]); // Run every time the messages array changes
+  };
+
+  useEffect(() => {
+    // Delayed scroll for initial load
+    const timer = setTimeout(() => {
+      scrollToBottom();
+    }, 3500); // Adjust delay as needed
+  
+    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+  }, []); // This effect runs once on mount
+
+  useEffect(() => {
+    // Immediate scroll when messages update
+    scrollToBottom();
+  }, [messages]); // This effect runs every time the messages array changes
 
   return (
-    <div ref={containerRef} className="space-y-2 overflow-auto p-3" style={{maxHeight:'65vh'}}>
+    <div ref={containerRef} className="space-y-2 overflow-auto p-3" style={{ maxHeight: '65vh' }}>
       {messages.map((msg) => (
         <MessageComponent key={msg.id} {...msg} />
       ))}
