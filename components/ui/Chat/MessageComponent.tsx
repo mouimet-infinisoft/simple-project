1042c258fcfd2a26ffd62f91f8c0fa86a49fcf76
Martin Ouimet <mouimet@infinisoft.world>
2024-02-26 09:11:28 -0500

    refactor processtext to handle backtick

--- components/ui/Chat/MessageComponent.tsx
+++ components/ui/Chat/MessageComponent.tsx
@@ -7,16 +7,14 @@ import dynamic from 'next/dynamic';
 // import remarkGfm from 'remark-gfm';
 // import { Remark } from 'react-remark';
 import { DiagramModule } from '@brainstack/diagram';
-import remarkRehype from 'remark-rehype'
+import remarkRehype from 'remark-rehype';
 // import rehypeStringify from 'rehype-stringify'
-import remarkGfm from 'remark-gfm'
-import remarkParse from 'remark-parse'
+import remarkGfm from 'remark-gfm';
+import remarkParse from 'remark-parse';
 // import {unified} from 'unified'
 
 // Dynamically import ReactMarkdown with SSR disabled
 const ReactMarkdown = dynamic(() => import('react-markdown'), { ssr: false });
-
-
 
 const MessageComponent = ({ text, role }: ChatMessage) => {
   const messageStyles: Record<string, string> = {
@@ -34,21 +32,23 @@ const MessageComponent = ({ text, role }: ChatMessage) => {
   // .use(rehypeStringify)
   // .process('# Hi\n\n*Hello*, world!')
 
-// console.log(String(file))
+  // console.log(String(file))
 
-  // Function to replace PlantUML code with the SVG URL
   const processText = useMemo(() => {
-    console.log(`MessageComponent ProcessText: `, text)
-    const plantUmlRegex = /@startuml([\s\S]*?)@enduml/g;
+    console.log(`MessageComponent ProcessText: `, text);
+    const plantUmlRegex = /```(?:\n)?@startuml([\s\S]*?)@enduml(?:\n)?```/g;
     return text.replace(plantUmlRegex, (match, p1) => {
-      const plantUmlCode = p1.trim(); // Remove @startuml and @enduml
-      const svgUrl = DiagramModule.generate_svg(plantUmlCode);
-      return `![PlantUML Diagram](${svgUrl})`; // Markdown image syntax
+      const plantUmlCode = p1.trim(); // Trim the PlantUML code
+      const svgUrl = DiagramModule.generate_svg(plantUmlCode); // Generate SVG URL
+      return `![PlantUML Diagram](${svgUrl})`; // Return Markdown image syntax
     });
   }, [text]);
-  //    <ReactMarkdown className={combinedStyles} remarkPlugins={[remarkGfm]}>
+
   return (
-    <ReactMarkdown className={combinedStyles} remarkPlugins={[remarkParse, remarkRehype, remarkGfm]}>
+    <ReactMarkdown
+      className={combinedStyles}
+      remarkPlugins={[remarkParse, remarkRehype, remarkGfm]}
+    >
       {processText}
     </ReactMarkdown>
   );
