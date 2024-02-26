9fe666e9b63b64589e21c97c41d44b8ba74e435e
Martin Ouimet <mouimet@infinisoft.world>
2024-02-25 23:26:43 -0500

    include diagram in chat

--- components/ui/Chat/MessageComponent.tsx
+++ components/ui/Chat/MessageComponent.tsx
@@ -1,25 +1,34 @@
+import React, { useMemo } from 'react';
 import { ChatMessage } from '@/types/ChatMessage';
 import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm'
+import remarkGfm from 'remark-gfm';
+import { DiagramModule } from '@brainstack/diagram';
 
 // components/MessageComponent.tsx
 
 const MessageComponent = ({ text, role }: ChatMessage) => {
-  // Conditional class names based on the role
   const messageStyles: Record<string, string> = {
     base: 'rounded-md p-2 mb-2 shadow text-white',
-    user: 'bg-blue-600 text-right', // Right align for "me" messages
-    ibrain: 'bg-green-600 text-left' // Left align for "ibrain" messages
+    user: 'bg-blue-600 text-right',
+    ibrain: 'bg-green-600 text-left',
   };
 
-  // Combine base styles with conditional styles
   const combinedStyles = `${messageStyles.base} ${messageStyles[role]}`;
 
+  // Function to replace PlantUML code with the SVG URL
+  const processText = useMemo(() => {
+    const plantUmlRegex = /@startuml([\s\S]*?)@enduml/g;
+    return text.replace(plantUmlRegex, (match, p1) => {
+      const plantUmlCode = p1.trim(); // Remove @startuml and @enduml
+      const svgUrl = DiagramModule.generate_svg(plantUmlCode);
+      return `![PlantUML Diagram](${svgUrl})`; // Markdown image syntax
+    });
+  }, [text]);
+
   return (
-    <ReactMarkdown className={combinedStyles} remarkPlugins={[remarkGfm]}>{text}</ReactMarkdown>
-    // <li className={combinedStyles}>
-    //   {role}:<ReactMarkdown className={combinedStyles}>{role}:{text}</ReactMarkdown>
-    // </li>
+    <ReactMarkdown className={combinedStyles} remarkPlugins={[remarkGfm]}>
+      {processText}
+    </ReactMarkdown>
   );
 };
 
