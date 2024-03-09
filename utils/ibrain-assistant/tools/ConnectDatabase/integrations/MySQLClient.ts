181a0c9a8200af8bea60ed681951b7ff5aa2ade3
Martin Ouimet <mouimet@infinisoft.world>
2024-03-09 07:03:24 -0500

    connect async

--- utils/ibrain-assistant/tools/ConnectDatabase/integrations/MySQLClient.ts
+++ utils/ibrain-assistant/tools/ConnectDatabase/integrations/MySQLClient.ts
@@ -1,11 +1,11 @@
-import { DatabaseLogin, DatabaseClient } from "../abstraction";
+import { DatabaseLogin, DatabaseClient } from '../abstraction';
 
 // MySQL database client
 
 export class MySQLClient implements DatabaseClient {
-  constructor(private login: DatabaseLogin) { }
+  constructor(private login: DatabaseLogin) {}
 
-  connect(): void {
+  async connect() {
     console.log('Connecting to MySQL database...');
     // Implement MySQL connection logic
   }
--- utils/ibrain-assistant/tools/ConnectDatabase/integrations/SQLClient.ts
+++ utils/ibrain-assistant/tools/ConnectDatabase/integrations/SQLClient.ts
@@ -1,11 +1,11 @@
-import { DatabaseLogin, DatabaseClient } from "../abstraction";
+import { DatabaseLogin, DatabaseClient } from '../abstraction';
 
 // SQL Server database client
 
 export class SQLClient implements DatabaseClient {
-  constructor(private login: DatabaseLogin) { }
+  constructor(private login: DatabaseLogin) {}
 
-  connect(): void {
+  async connect() {
     console.log('Connecting to SQL Server database...');
     // Implement SQL Server connection logic
   }
