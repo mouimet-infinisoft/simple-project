de09dfff9063414c408f969611eb551973ace042
Martin Ouimet <mouimet@infinisoft.world>
2024-04-01 19:40:59 -0400

    .

--- app/protected/onboarding/page.tsx
+++ app/protected/onboarding/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import AIForm from '@/components/ui/AccountForms/AIForm';
 import { createClient } from '@/utils/supabase/server';
+import useAuthorization from '@/app/hooks/useAuthorization';
 
 export default async function OnboardingPage() {
+  useAuthorization();
   const supabase = createClient();
 
   const {
@@ -15,28 +17,21 @@ export default async function OnboardingPage() {
     .eq('id', user?.id ?? '')
     .single();
 
-  const { data: subscription, error } = await supabase
-    .from('subscriptions')
-    .select('*, prices(*, products(*))')
-    .in('status', ['trialing', 'active'])
-    .maybeSingle();
-
   return (
     <>
-
-        <div className="p-4 min-h-screen">
-          <div className="max-w-6xl mx-auto">
-            <div className="text-3xl font-bold text-center mb-10">
-              Onboarding
-              <AIForm
-                apiKey={userDetails?.openai_apikey ?? ''}
-                assistantId={userDetails?.assistant_id ?? ''}
-                togetherApiKey={userDetails?.togetherai_apikey ?? ''}
-                aiIntegration={userDetails?.ai_integration ?? ''}
-              />
-            </div>
+      <div className="p-4 min-h-screen">
+        <div className="max-w-6xl mx-auto">
+          <div className="text-3xl font-bold text-center mb-10">
+            Onboarding
+            <AIForm
+              apiKey={userDetails?.openai_apikey ?? ''}
+              assistantId={userDetails?.assistant_id ?? ''}
+              togetherApiKey={userDetails?.togetherai_apikey ?? ''}
+              aiIntegration={userDetails?.ai_integration ?? ''}
+            />
           </div>
         </div>
+      </div>
     </>
   );
 }
